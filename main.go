f89eb4d54dbe8a3a8a0eeb0ef935c920537e6db8
JampOreo <joaquinmedinapedrozo@gmail.com>
2026-08-26 10:00:44 -0300

    v1.0, funcional y compilable

--- main.go
+++ main.go
@@ -17,7 +17,7 @@ func main() {
 
 	// Create application with options
 	err := wails.Run(&options.App{
-		Title:  "appdev",
+		Title:  "Calculadora 3D",
 		Width:  1024,
 		Height: 768,
 		AssetServer: &assetserver.Options{
